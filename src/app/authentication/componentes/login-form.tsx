d08b9eeae9a3a66b5e7d542de3cc474f7b6ae761
Lucas de Sousa Silva <lucas.sousa07@aluno.ifce.edu.br>
2025-05-29 10:58:09 -0300

    feat: add login

--- src/app/authentication/componentes/login-form.tsx
+++ src/app/authentication/componentes/login-form.tsx
@@ -21,6 +21,10 @@ import {
 import { useForm } from "react-hook-form";
 import { z } from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
+import { authClient } from "@/lib/auth-client";
+import { useRouter } from "next/navigation";
+import { toast } from "sonner";
+import { Loader2Icon } from "lucide-react";
 
 const loginSchema = z.object({
   email: z
@@ -36,6 +40,8 @@ const loginSchema = z.object({
 });
 
 const LoginForm = () => {
+  const router = useRouter();
+
   const form = useForm<z.infer<typeof loginSchema>>({
     resolver: zodResolver(loginSchema),
     defaultValues: {
@@ -44,10 +50,22 @@ const LoginForm = () => {
     },
   });
 
-  const onSubmit = (values: z.infer<typeof loginSchema>) => {
-    // Do something with the form values.
-    // ✅ This will be type-safe and validated.
-    console.log(values);
+  const onSubmit = async (values: z.infer<typeof loginSchema>) => {
+    await authClient.signIn.email(
+      {
+        email: values.email,
+        password: values.password,
+      },
+      {
+        onSuccess: () => {
+          toast.success("Login realizado com sucesso!");
+          router.push("/dashboard");
+        },
+        onError: () => {
+          toast.error("Email ou senha inválidos!");
+        },
+      },
+    );
   };
 
   return (
@@ -94,8 +112,16 @@ const LoginForm = () => {
           </CardContent>
 
           <CardFooter>
-            <Button type="submit" className="w-full cursor-pointer">
-              Entrar
+            <Button
+              type="submit"
+              className="w-full cursor-pointer"
+              disabled={form.formState.isSubmitting}
+            >
+              {form.formState.isSubmitting ? (
+                <Loader2Icon className="mr-4 size-4 animate-spin" />
+              ) : (
+                "Entrar"
+              )}
             </Button>
           </CardFooter>
         </form>
