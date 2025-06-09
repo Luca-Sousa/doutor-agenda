1acbd78f9159e14f21e709fb585cd2ded8d23abd
Lucas de Sousa Silva <lucas.sousa07@aluno.ifce.edu.br>
2025-06-09 11:00:51 -0300

    feat: ensure that the production build is performed correctly

---
+++ drizzle/0001_sticky_boom_boom.sql
@@ -0,0 +1,4 @@
+ALTER TABLE "appointments" ADD COLUMN "appointment_price_in_cents" integer NOT NULL;--> statement-breakpoint
+ALTER TABLE "users" ADD COLUMN "stripe_customer_id" text;--> statement-breakpoint
+ALTER TABLE "users" ADD COLUMN "stripe_subscription_id" text;--> statement-breakpoint
+ALTER TABLE "users" ADD COLUMN "plan" text;
